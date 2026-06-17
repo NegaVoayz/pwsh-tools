3fc0ffdd1c4cbf90c90158e0da58cc2a41a03487
NegaVoayz <Nega_Voayz@outlook.com>
2026-06-17 16:18:05 +0800

    Remove test init.psm1 from man package

--- lib/man/init.psm1
+++
@@ -1,4 +0,0 @@
-# man/init.psm1 -- Init module for man package.
-# Imported by bootstrap/init.ps1 on every shell start.
-
-Write-Verbose "[man] Help system ready -- use 'Show-Manual' to see available commands."
